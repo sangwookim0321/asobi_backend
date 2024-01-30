d46d7050506498d70dbc53e10d71992a6e21ffac
하디 <pointjumpit@coinvest.kr>
2024-01-30 13:56:37 +0900

    prod, dev 서버 실행 분리

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
 const dotenv = require('dotenv')
 const express = require('express')
 const cors = require('cors')
+const fs = require('fs')
+const https = require('https')
+const http = require('http')
 const app = express()
 
+// 환경 설정 파일 로드
 if (process.env.NODE_ENV === 'production') {
     dotenv.config({ path: '.env.production' })
 } else {
-    dotenv.config({ path: '.env.development'})
+    dotenv.config({ path: '.env.development' })
 }
 
 const PORT = process.env.PORT || 9002
@@ -14,14 +18,26 @@ const PORT = process.env.PORT || 9002
 app.use(express.json())
 const corsOptions = {
     origin: 'https://asobi-hardy.vercel.app',
-    // credentials: true, // 인증 정보를 요청과 함께 보낼지 여부
     optionSuccessStatus: 200,
 }
-app.use(cors(corsOptions)) // 프론트 도메인으로 변경
+app.use(cors(corsOptions))
 
 const gptApiRouter = require('./routes/gptApi')
 app.use('/gpt-api', gptApiRouter)
 
-app.listen(PORT, (req, res) => {
-	console.log(`Server is running on port ${PORT}.`)
-})
+// 프로덕션 환경에서는 HTTPS 서버 실행
+if (process.env.NODE_ENV === 'production') {
+    const options = {
+        key: fs.readFileSync('/etc/letsencrypt/live/pointjumpit.duckdns.org/privkey.pem'),
+        cert: fs.readFileSync('/etc/letsencrypt/live/pointjumpit.duckdns.org/fullchain.pem')
+    }
+
+    https.createServer(options, app).listen(PORT, () => {
+        console.log(`Prod HTTPS Server is running on port ${PORT}.`)
+    })
+} else {
+    // 개발 환경에서는 HTTP 서버 실행
+    http.createServer(app).listen(PORT, () => {
+        console.log(`Dev HTTP Server is running on port ${PORT}.`)
+    })
+}
